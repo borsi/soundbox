b6baed82103aa036dd48efb16da08e9b95899e79
borsi <deadnervster@gmail.com>
2018-06-13 21:45:56 +0200

    Added folder read

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import serial
 import os
+from os import listdir
+from os.path import isfile, join
+mypath = "sounds"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
+print(onlyfiles)
 
 PORT = "/dev/ttyACM0"
 BAUD =  115200
