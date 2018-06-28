be4f5cef520d7e9219aa8f46657a5f270538b38b
borsi <deadnervster@gmail.com>
2018-06-28 18:39:18 +0000

    button control works

--- button_control.py
+++ button_control.py
@@ -1,6 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 
+import os
+from os import listdir
+from os.path import isfile, join
+import random
+
+mypath = "/home/pi/soundbox/sounds"
+onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
@@ -8,5 +16,8 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state = GPIO.input(18)
     if input_state == False:
+        index = random.randint(0, len(onlyfiles)-1)
+        selected = onlyfiles[index]
+        os.system('aplay '+mypath+'/'+selected)
         print('Button Pressed')
         time.sleep(0.2)
